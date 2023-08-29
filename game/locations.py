c12b05184a6daaf0e9801fa047bc618ab8ecc088
shanerob1106 <shanerob11062@gmail.com>
2023-08-29 21:09:05 +0100

    Added shared_interactions for locations

--- game/locations.py
+++ game/locations.py
@@ -1,27 +1,36 @@
 from game.location import Location
 
+shared_interactions = {
+    "move": "You can move to a new area.",
+    "back": "Back to main menu."
+}
+
 # Create some locations
 village = Location(
     name="Village",
     description="A peaceful village with bustling activity.",
-    interactions={"move": "You can move to the forest or the cave.",
-                  "rest": "You can rest to restore your health.",
-                  "shop": "You can visit the shop to buy and sell items.",
-                  "talk": "You can talk to the villagers.",
-                  "back": "Back to main menu." },
+    interactions={
+        **shared_interactions,
+        "rest": "You can rest to restore your health.",
+        "shop": "You can visit the shop to buy and sell items.",
+        "talk": "You can talk to the villagers." },
     #items=[Item("Potion", "Restores a small amount of health.")]
 )
 
 forest = Location(
     name="Forest",
     description="A dense forest with towering trees.",
-    interactions={"search": "You search the area for useful items."},
+    interactions={
+        **shared_interactions,
+        "search": "You search the area for useful items."},
     #items=[Item("Herb", "A medicinal herb with healing properties.")]
 )
 
 cave = Location(
     name="Cave",
     description="A dark and eerie cave entrance.",
-    interactions={"enter": "You cautiously enter the cave."},
+    interactions={
+        **shared_interactions,
+        "enter": "You cautiously enter the cave."},
     #items=[Item("Torch", "Provides light in dark places.")]
 )
